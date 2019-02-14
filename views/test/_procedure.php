73823eec4b76d38519cadecc01ca53ab741d2e0d
bugkot40 <konstantin.programmer71@gmail.com>
2019-02-14 15:14:49 +0300

    добавил стилей

--- views/test/_procedure.php
+++ views/test/_procedure.php
@@ -2,14 +2,27 @@
 use yii\helpers\Url;
 
 ?>
-<h1>Обучение</h1>
+<h1>Обучение . <?= $question-> ?></h1>
 
 <?php if ($question): ?>
     <a class="js_test" href="<?= Url::toRoute(['test/procedure-next', 'testId' => $question->test_id]) ?>"
        data-url='test/procedure-next' data-id="<?= $question->test_id ?>">
         Продолжить
     </a>
-    <?php debug($question) ?>
+
+	<div id="link">
+		<?='['. $question->link. ']' ?>
+	</div>
+	<div id="question">
+		<?= $question->question ?>
+	</div>
+	<div id="answer">
+		<?= $question->answer ?>
+	</div>
+
+
+
+
 <?php else: ?>
     <h2>Вопросы кончились</h2>
 <?php endif; ?>
