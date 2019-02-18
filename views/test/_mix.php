68387a4a75bf5878723b0822346bab91b263238d
bugkot40 <konstantin.programmer71@gmail.com>
2019-02-18 21:01:40 +0300

    bag

--- views/test/_mix.php
+++ views/test/_mix.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 <?php if ($question): ?>
     <h1>Супер-тест</h1>
-    <p><?= $question->test->name?></p>
+    <p><?= $question->test->name ?></p>
     <?php $test = $question->test; ?>
     <a class="js_test next" href="<?= Url::toRoute(['test/mix-next', 'mixId' => $test->mix_id]) ?>"
        data-url='test/mix-next' data-id="<?= $test->mix_id ?>">
@@ -23,13 +23,12 @@ use yii\helpers\Html;
     <div id="answer" class="vis_answer">
         <?= $question->answer ?>
     </div>
-	<?php if ($question->image) : ?>
-		<div id="image">
-			<?php $url = '@web/images/questions/'.$question->image ?>
-			<?= Html::img($url, ['alt' => 'image', 'width' => '90%', 'position' => 'center-center']) ?>
-		</div>
-		<?php endif; ?>
-	<?php else: ?>
+    <?php if ($question->image) : ?>
+        <div id="image">
+            <?php $url = '@web/images/questions/' . $question->image ?>
+            <?= Html::img($url, ['alt' => 'image', 'width' => '90%', 'position' => 'center-center']) ?>
+        </div>
+    <?php endif; ?>
 <?php else: ?>
     <h2>Вопросы кончились</h2>
 <?php endif; ?>
